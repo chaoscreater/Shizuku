c7a8e14513c742fe274b910c235a7e88abf8f309
chaoscreater <chaoscreater@gmail.com>
2026-03-10 03:23:12 +1300

    Added a bunch of new features

---
+++ manager/src/main/java/moe/shizuku/manager/receiver/ManualStartReceiver.kt
@@ -0,0 +1,15 @@
+package moe.shizuku.manager.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import moe.shizuku.manager.BuildConfig
+
+class ManualStartReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val applicationId = BuildConfig.APPLICATION_ID
+        if (intent.action != "${applicationId}.START") return
+
+        ShizukuReceiverStarter.start(context)
+    }
+}
